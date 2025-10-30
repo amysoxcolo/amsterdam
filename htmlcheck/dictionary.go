05a43bcd476d3db2e99a14980ecc4a56e91b2242
Amy Gale Ruth Bowersox <amy@erbosoft.com>
2025-10-29 23:16:21 -0600

    starting to sketch out the dictionary interfaces for spell checking

---
+++ htmlcheck/dictionary.go
@@ -0,0 +1,23 @@
+/*
+ * Amsterdam Web Communities System
+ * Copyright (c) 2025 Erbosoft Metaverse Design Solutions, All Rights Reserved
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+// The htmlcheck package contains the HTML Checker.
+package htmlcheck
+
+type SpellingDictionary interface {
+	Ready() bool
+	Size() int
+	CheckWord(string) bool
+}
+
+type ModSpellingDictionary interface {
+	SpellingDictionary
+	AddWord(string)
+	DelWord(string)
+	Clear()
+}
