7e34c33e5802eab5f3e32f078596d3f08e5f8cc2
Amy Gale Ruth Bowersox <amy@erbosoft.com>
2026-03-12 11:28:16 -0600

    ran "go fix" and it couldn't find much to do

--- logging.go
+++ logging.go
@@ -81,34 +81,34 @@ func fromglog(l glog.Lvl) log.Level {
 // EchoLogrusAdapter implements echo.Logger using logrus.
 type EchoLogrusAdapter struct{}
 
-func (l *EchoLogrusAdapter) Output() io.Writer                         { return log.StandardLogger().Out }
-func (l *EchoLogrusAdapter) SetOutput(w io.Writer)                     { log.SetOutput(w) }
-func (l *EchoLogrusAdapter) Prefix() string                            { return "" }
-func (l *EchoLogrusAdapter) SetPrefix(p string)                        {}
-func (l *EchoLogrusAdapter) Level() glog.Lvl                           { return toglog(log.GetLevel()) }
-func (l *EchoLogrusAdapter) SetLevel(lvl glog.Lvl)                     { log.SetLevel(fromglog(lvl)) }
-func (l *EchoLogrusAdapter) Print(i ...interface{})                    { log.Print(i...) }
-func (l *EchoLogrusAdapter) Printf(format string, args ...interface{}) { log.Printf(format, args...) }
-func (l *EchoLogrusAdapter) Printj(j glog.JSON)                        { log.WithFields(log.Fields(j)).Print() }
-func (l *EchoLogrusAdapter) Debug(i ...interface{})                    { log.Debug(i...) }
-func (l *EchoLogrusAdapter) Debugf(format string, args ...interface{}) { log.Debugf(format, args...) }
-func (l *EchoLogrusAdapter) Debugj(j glog.JSON)                        { log.WithFields(log.Fields(j)).Debug() }
-func (l *EchoLogrusAdapter) Info(i ...interface{})                     { log.Info(i...) }
-func (l *EchoLogrusAdapter) Infof(format string, args ...interface{})  { log.Infof(format, args...) }
-func (l *EchoLogrusAdapter) Infoj(j glog.JSON)                         { log.WithFields(log.Fields(j)).Info() }
-func (l *EchoLogrusAdapter) Warn(i ...interface{})                     { log.Warn(i...) }
-func (l *EchoLogrusAdapter) Warnf(format string, args ...interface{})  { log.Warnf(format, args...) }
-func (l *EchoLogrusAdapter) Warnj(j glog.JSON)                         { log.WithFields(log.Fields(j)).Warn() }
-func (l *EchoLogrusAdapter) Error(i ...interface{})                    { log.Error(i...) }
-func (l *EchoLogrusAdapter) Errorf(format string, args ...interface{}) { log.Errorf(format, args...) }
-func (l *EchoLogrusAdapter) Errorj(j glog.JSON)                        { log.WithFields(log.Fields(j)).Error() }
-func (l *EchoLogrusAdapter) Fatal(i ...interface{})                    { log.Fatal(i...) }
-func (l *EchoLogrusAdapter) Fatalf(format string, args ...interface{}) { log.Fatalf(format, args...) }
-func (l *EchoLogrusAdapter) Fatalj(j glog.JSON)                        { log.WithFields(log.Fields(j)).Fatal() }
-func (l *EchoLogrusAdapter) Panic(i ...interface{})                    { log.Panic(i...) }
-func (l *EchoLogrusAdapter) Panicf(format string, args ...interface{}) { log.Panicf(format, args...) }
-func (l *EchoLogrusAdapter) Panicj(j glog.JSON)                        { log.WithFields(log.Fields(j)).Panic() }
-func (l *EchoLogrusAdapter) SetHeader(h string)                        {}
+func (l *EchoLogrusAdapter) Output() io.Writer                 { return log.StandardLogger().Out }
+func (l *EchoLogrusAdapter) SetOutput(w io.Writer)             { log.SetOutput(w) }
+func (l *EchoLogrusAdapter) Prefix() string                    { return "" }
+func (l *EchoLogrusAdapter) SetPrefix(p string)                {}
+func (l *EchoLogrusAdapter) Level() glog.Lvl                   { return toglog(log.GetLevel()) }
+func (l *EchoLogrusAdapter) SetLevel(lvl glog.Lvl)             { log.SetLevel(fromglog(lvl)) }
+func (l *EchoLogrusAdapter) Print(i ...any)                    { log.Print(i...) }
+func (l *EchoLogrusAdapter) Printf(format string, args ...any) { log.Printf(format, args...) }
+func (l *EchoLogrusAdapter) Printj(j glog.JSON)                { log.WithFields(log.Fields(j)).Print() }
+func (l *EchoLogrusAdapter) Debug(i ...any)                    { log.Debug(i...) }
+func (l *EchoLogrusAdapter) Debugf(format string, args ...any) { log.Debugf(format, args...) }
+func (l *EchoLogrusAdapter) Debugj(j glog.JSON)                { log.WithFields(log.Fields(j)).Debug() }
+func (l *EchoLogrusAdapter) Info(i ...any)                     { log.Info(i...) }
+func (l *EchoLogrusAdapter) Infof(format string, args ...any)  { log.Infof(format, args...) }
+func (l *EchoLogrusAdapter) Infoj(j glog.JSON)                 { log.WithFields(log.Fields(j)).Info() }
+func (l *EchoLogrusAdapter) Warn(i ...any)                     { log.Warn(i...) }
+func (l *EchoLogrusAdapter) Warnf(format string, args ...any)  { log.Warnf(format, args...) }
+func (l *EchoLogrusAdapter) Warnj(j glog.JSON)                 { log.WithFields(log.Fields(j)).Warn() }
+func (l *EchoLogrusAdapter) Error(i ...any)                    { log.Error(i...) }
+func (l *EchoLogrusAdapter) Errorf(format string, args ...any) { log.Errorf(format, args...) }
+func (l *EchoLogrusAdapter) Errorj(j glog.JSON)                { log.WithFields(log.Fields(j)).Error() }
+func (l *EchoLogrusAdapter) Fatal(i ...any)                    { log.Fatal(i...) }
+func (l *EchoLogrusAdapter) Fatalf(format string, args ...any) { log.Fatalf(format, args...) }
+func (l *EchoLogrusAdapter) Fatalj(j glog.JSON)                { log.WithFields(log.Fields(j)).Fatal() }
+func (l *EchoLogrusAdapter) Panic(i ...any)                    { log.Panic(i...) }
+func (l *EchoLogrusAdapter) Panicf(format string, args ...any) { log.Panicf(format, args...) }
+func (l *EchoLogrusAdapter) Panicj(j glog.JSON)                { log.WithFields(log.Fields(j)).Panic() }
+func (l *EchoLogrusAdapter) SetHeader(h string)                {}
 
 /*----------------------------------------------------------------------------
  * Echo middleware adapters
