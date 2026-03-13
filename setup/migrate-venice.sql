2526534ad7fe17a8d77bb482f051ee8f683d02f3
Amy Gale Ruth Bowersox <amy@erbosoft.com>
2026-03-13 14:31:45 -0600

    added SPDX identifiers to email, exports, and htmlcheck packages, and to the setup database scripts

--- setup/migrate-venice.sql
+++ setup/migrate-venice.sql
@@ -8,6 +8,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
+# SPDX-License-Identifier: MPL-2.0
+#
 
 ALTER TABLE globals RENAME COLUMN max_sig_mbr_page TO max_comm_mbr_page;
 ALTER TABLE globals RENAME COLUMN sig_create_lvl TO comm_create_lvl;
